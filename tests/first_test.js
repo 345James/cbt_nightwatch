1f51ccb3da3db98dba9339626fb5d770da7424a9
ritikrishu <ritik.rishu@hotcocoasoftware.com>
2019-04-17 16:11:58 +0530

    config changes as suggested by CBT team

--- tests/first_test.js
+++ tests/first_test.js
@@ -1,7 +1,6 @@
 this.toDos = function (browser) {
   browser
     .url('http://localhost:8080/')
-    .useXpath()
-    .waitForElementVisible('#test', 4000)
+    .waitForElementVisible('#test', 30000)
     .expect.element('#test').text.to.equal('Welcome to Your Vue.js App');
 };
